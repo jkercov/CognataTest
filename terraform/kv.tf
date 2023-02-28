3b709ff70b8d2ef8cca9d249dedb68cc1b9c5702
Jovan Kercov <j.kercov@vegait.rs>
2023-02-28 13:18:47 +0100

    Adding KV to TF

---
+++ terraform/kv.tf
@@ -0,0 +1,32 @@
+resource "azurerm_key_vault" "kv" {
+  name                       = "kv-${var.environment}"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "premium"
+  soft_delete_retention_days = 7
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Set",
+      "Get",
+      "Delete",
+      "Purge",
+      "Recover"
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "example" {
+  name         = "somesecret"
+  value        = "somevalue"
+  key_vault_id = azurerm_key_vault.example.id
+}
